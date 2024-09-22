bab394de0a8ce421f3bc779b54294460e143fe09
Aryan Kumer <aryandot1@gmail.com>
2024-09-22 05:31:26 +0530

    added frontend files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import JobListing from './pages/JobListing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import JobDetail from './pages/JobDetail';
+import RegisterPage from './pages/Register';
+import LoginPage from './pages/Login';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
+import { UserProvider } from './context/UserContext'; // Import UserProvider
+import JobSeeker from './pages/JobSeeker';
+import EmployeeSeeker from './pages/EmployeeSeeker';
+import CreateJob from './pages/CreateJob';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <UserProvider> {/* Wrap the app with UserProvider */}
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/" element={<JobListing />} />
+            <Route path='/jobs/getJobById/:id' element={<JobDetail />} />
+            <Route path="/jobseeker" element={<JobSeeker />} />
+            {/* <Route path="/create-job" element={<CreateJob />} /> */}
+            <Route path='/employeeseeker' element={<EmployeeSeeker />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 
